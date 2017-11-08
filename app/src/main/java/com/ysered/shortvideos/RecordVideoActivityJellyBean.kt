0f923608a3ed563d80e056baebdb574ccc294584
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-11-08 23:22:33 +0200

    Implement camera switch.

--- app/src/main/java/com/ysered/shortvideos/RecordVideoActivityJellyBean.kt
+++ app/src/main/java/com/ysered/shortvideos/RecordVideoActivityJellyBean.kt
@@ -5,13 +5,14 @@ package com.ysered.shortvideos
 import android.hardware.Camera
 import android.os.Bundle
 import android.view.SurfaceHolder
-import com.ysered.shortvideos.utils.getBackCameraId
 import kotlinx.android.synthetic.main.activity_record_video.*
 
 
 class RecordVideoActivityJellyBean : BaseRecordVideoActivity(), SurfaceHolder.Callback {
 
     private var camera: Camera? = null
+    private var currentCameraId = Camera.CameraInfo.CAMERA_FACING_BACK
+    private var isPreviewing = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +22,16 @@ class RecordVideoActivityJellyBean : BaseRecordVideoActivity(), SurfaceHolder.Ca
             setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS)
         }
 
-        camera = Camera.open(getBackCameraId())
+        switchCameraButton.setOnClickListener {
+            restartPreview(isSwitch = true)
+            showPreview(surfaceView.holder)
+        }
+
+        restartPreview()
     }
 
     override fun surfaceCreated(surfaceHolder: SurfaceHolder?) {
-        camera?.apply {
-            setPreviewDisplay(surfaceHolder)
-            startPreview()
-        }
+        showPreview(surfaceHolder)
     }
 
     override fun surfaceChanged(surfaceHolder: SurfaceHolder?, format: Int, width: Int, height: Int) = Unit
@@ -38,5 +41,32 @@ class RecordVideoActivityJellyBean : BaseRecordVideoActivity(), SurfaceHolder.Ca
             stopPreview()
             release()
         }
+    }
+
+    private fun showPreview(surfaceHolder: SurfaceHolder?) {
+        camera?.apply {
+            setPreviewDisplay(surfaceHolder)
+            startPreview()
+            isPreviewing = true
+        }
+    }
+
+    private fun restartPreview(isSwitch: Boolean = false) {
+        if (isSwitch) {
+            currentCameraId = if (currentCameraId == Camera.CameraInfo.CAMERA_FACING_BACK)
+                Camera.CameraInfo.CAMERA_FACING_FRONT
+            else
+                Camera.CameraInfo.CAMERA_FACING_BACK
+        }
+        camera?.apply {
+            if (isPreviewing)
+                stopPreview()
+            release()
+        }
+        camera = Camera.open(currentCameraId)
+    }
+
+    private fun switchCameraId() {
+
     }
 }

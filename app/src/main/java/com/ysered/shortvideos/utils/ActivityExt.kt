e64a8b64889280e65cd335b7cc44a325797710b6
Yuriy Seredyuk <yuriy.seredyuk@gmail.com>
2017-11-09 13:27:34 +0200

    Handle else branch properly.

--- app/src/main/java/com/ysered/shortvideos/utils/ActivityExt.kt
+++ app/src/main/java/com/ysered/shortvideos/utils/ActivityExt.kt
@@ -8,11 +8,12 @@ import android.view.Surface
 
 val Activity.rotationDegrees: Int
     get() {
-        return when (windowManager.defaultDisplay.rotation) {
+        val rotation = windowManager.defaultDisplay.rotation
+        return when (rotation) {
             Surface.ROTATION_0 -> 0
             Surface.ROTATION_90 -> 90
             Surface.ROTATION_180 -> 180
             Surface.ROTATION_270 -> 270
-            else -> 0
+            else -> throw RuntimeException("rotation should be one of: 0, 90, 180 or 270, but actually was: $rotation")
         }
     }
